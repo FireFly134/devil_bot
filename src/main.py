665e68a3c0d1ff166cdc0df156674b029a882ed7
konstantin.tkachev <konstantin.tkachev@webpp.ru>
2024-10-17 23:49:35 +0300

    Добавил простую регистрацию

--- src/main.py
+++ src/main.py
@@ -1,26 +1,131 @@
 import asyncio
+import logging
 import os
 from typing import Any
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, F, Router, html, types
 from aiogram.client.default import DefaultBotProperties
 from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.filters.command import Command
-from aiogram.types import FSInputFile
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import (
+    CallbackQuery,
+    FSInputFile,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    KeyboardButton,
+    ReplyKeyboardMarkup,
+)
 
 from config import settings
 from tables.heroes_of_users import HeroesOfUsers
 from tables.telegram_users import User
 
-dp = Dispatcher()
+# from buttons import setting_hero_button
+
+form_router = Router()
 
 
-@dp.message(CommandStart())
-async def start(message: types.Message) -> None:
-    # user_id = message.from_user.id
-    await message.answer("Привет, меня зовут Люцик!")
+class Regisration(StatesGroup):
+    name = State()
+    user_id = State()
+    hero_id = State()
+
+
+async def first_sms(message: types.Message):
+    sms = """Сейчас время смены кланового задания установлено __18:30__, а первый сбор бесплатной энергии установлен на __12:00__ \(__*по МСК*__\)\.
+
+    *Если данное время неверно, то это можно с лёгкостью изменить в настройках\!*
+    Для этого нажми *Настройка профиля* \-\-\-\> *Поменять время\.\.\.*
+
+    *Так же можно __бесплатно__ подписаться на напоминалки\!*
+    Чтобы это сделать проходим *Настройка профиля* \-\-\-\> *Подписки\.\.\.*
+    """
+    await message.answer(sms, parse_mode=ParseMode.MARKDOWN_V2)
+
+
+@form_router.message(CommandStart())
+async def start(message: types.Message, state: FSMContext) -> None:
+    if message.chat.type == "private":
+        if message.from_user is not None:
+            user_id: int = int(message.from_user.id)
+            if user := await User.query.where(
+                User.user_id == user_id
+            ).gino.first():
+                if hero_user_search := await HeroesOfUsers.query.where(
+                    HeroesOfUsers.user_id == user.id
+                ).gino.first():
+                    sms = f"Привет, {str(hero_user_search.name)}"
+                    await message.answer(sms)
+                    return
+                # user(message, sms)
+            else:
+                user = await User(
+                    user_id=user_id,
+                    first_name=message.from_user.first_name,
+                    last_name=message.from_user.last_name,
+                    username=message.from_user.username,
+                    language_code=message.from_user.language_code,
+                ).create()
+                await message.answer(
+                    "Я тебя не помню. Давай знакомиться! Какой у тебя ник в игре?"
+                )
+            await state.update_data(user_id=user.id)
+            await state.set_state(Regisration.name)
+
+
+@form_router.callback_query(Regisration.name, F.data == "yes")
+async def missing_name(call: CallbackQuery, state: FSMContext) -> None:
+    data = await state.get_data()
+    if "hero_id" in data:
+        hero = await HeroesOfUsers.query.where(
+            HeroesOfUsers.id == data["hero_id"]
+        )
+        hero.update(name=data["name"]).apply()
+    else:
+        await HeroesOfUsers(
+            user_id=data["user_id"],
+            name=data["name"],
+        ).create()
+    await state.clear()
+    await call.message.delete()
+    # setting_hero_button(
+    #     message, "Отлично, будем знакомы)"
+    # )
+    await call.message.answer("Отлично, будем знакомы)")
+    try:
+        await first_sms(call.message)
+    except Exception as err:
+        logging.error(err)
+        logging.info(f"Пользователь с id = {call.from_user.id}")
+
+
+@form_router.callback_query(Regisration.name, F.data == "no")
+async def missing_name(call: CallbackQuery) -> None:
+    await call.message.delete()
+    await call.message.answer(
+        "Ок, давай попробуем снова. Какой у тебя ник в игре?"
+    )
+
+
+@form_router.message(Regisration.name)
+async def reg_start(message: types.Message, state: FSMContext) -> None:
+    await state.update_data(name=message.text)
+    await message.answer(
+        f'Ты герой под ником "{message.text}"?',
+        reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(text="Да", callback_data="yes"),
+                    InlineKeyboardButton(text="Нет", callback_data="no"),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
+    )
 
 
 async def set_default_commands(bot: Bot) -> None:
@@ -39,9 +144,14 @@ async def set_default_commands(bot: Bot) -> None:
 async def main() -> None:
     # Initialize Bot instance with a default parse mode which will be passed
     # to all API calls
-    bot = Bot(token=settings.TOKEN, session=AiohttpSession())
-    # bot = Bot(settings.TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    # bot = Bot(token=settings.TOKEN, session=AiohttpSession())
+    bot = Bot(
+        token=settings.TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
     await set_default_commands(bot)
+    dp = Dispatcher()
+    dp.include_router(form_router)
     # And the run events dispatching
     await dp.start_polling(bot)
 
