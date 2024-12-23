0b05321184d006e2963465b1594594ff416af019
konstantin.tkachev <konstantin.tkachev@webpp.ru>
2024-12-23 15:53:28 +0300

    Попытка запустить тестовую БД

---
+++ tests/test_bd.py
@@ -0,0 +1,24 @@
+import pytest
+
+from tables.telegram_users import User
+
+from tests.factories import UserFactory
+
+@pytest.mark.asyncio
+async def test_create_user():
+    print('test_create_user')
+    user = await UserFactory()
+    # assert user.send_msg
+
+
+@pytest.mark.asyncio
+async def test_read_user():
+    # Создаем тестовые данные
+    await User.create(name="Alice")
+    await User.create(name="Bob")
+
+    # Читаем
+    users = await User.query.gino.all()
+    assert len(users) == 2
+    assert users[0].name == "Alice"
+    assert users[1].name == "Bob"
