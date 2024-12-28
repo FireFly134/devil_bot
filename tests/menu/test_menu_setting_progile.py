3b599b23c36efe3c29cf8046d9b14f847b16f869
konstantin.tkachev <konstantin.tkachev@webpp.ru>
2024-12-28 17:15:19 +0300

    .

---
+++ tests/menu/test_menu_setting_progile.py
@@ -0,0 +1,10 @@
+from unittest.mock import AsyncMock
+
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_check_data_profile(mock_message: AsyncMock) -> None:
+    """Тестирование функции отображение данных о профиле."""
+
+
