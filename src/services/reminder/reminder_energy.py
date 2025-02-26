921c988c09167bad38fc620a437910c6fc8ed1dc
konstantin.tkachev <konstantin.tkachev@webpp.ru>
2025-02-26 20:47:43 +0300

    Исправил опечатку из легаси-кода.

--- src/services/reminder/reminder_energy.py
+++ src/services/reminder/reminder_energy.py
@@ -5,25 +5,34 @@ from sqlalchemy import and_, or_
 
 from services.send_message import send_msg_mv2
 from tables.heroes_of_users import HeroesOfUsers
+from tables.telegram_users import User
 
 
 async def reminder_energy():
     time = datetime.now()
     time2_energy = time - timedelta(hours=6)
     time3_energy = time - timedelta(hours=9)
-    heroes = await HeroesOfUsers.query.where(
-        and_(
-            or_(
-                HeroesOfUsers.time_collection_energy
-                == int(time.strftime("%H")),
-                HeroesOfUsers.time_collection_energy
-                == int(time2_energy.strftime("%H")),
-                HeroesOfUsers.time_collection_energy
-                == int(time3_energy.strftime("%H")),
-            ),
-            HeroesOfUsers.subscription_energy,
+    heroes = (
+        await HeroesOfUsers.join(User)
+        .select()
+        .where(
+            and_(
+                or_(
+                    HeroesOfUsers.time_collection_energy
+                    == int(time.strftime("%H")),
+                    HeroesOfUsers.time_collection_energy
+                    == int(time2_energy.strftime("%H")),
+                    HeroesOfUsers.time_collection_energy
+                    == int(time3_energy.strftime("%H")),
+                ),
+                HeroesOfUsers.subscription_energy,
+            )
         )
-    ).gino.all()
+        .with_only_columns(
+            (HeroesOfUsers.id, HeroesOfUsers.name, User.user_id)
+        )
+        .gino.all()
+    )
     for hero in heroes:
         name = (
             str(hero.name)
